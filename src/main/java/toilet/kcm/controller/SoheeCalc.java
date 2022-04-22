2bf1f7464b7970036c1b9dcd9372aae6b49cb5fe
soheekim <sohee417@gmail.com>
2022-04-22 22:18:17 +0900

    (20220421) 김소희 HW01 계산기만들기

--- src/main/java/toilet/kcm/controller/SoheeCalc.java
+++ src/main/java/toilet/kcm/controller/SoheeCalc.java
@@ -5,41 +5,39 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 import java.util.Scanner;
 
 public class SoheeCalc {
-
     public static void main(String[] args){
-        System.out.println("계산기 만들기");
-
         int a = 0;
         int b = 0;
-
         String sign = "";
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("첫번째 숫자 : ");
-        a = scan.nextInt();
+        System.out.println("계산기 만들기");
 
-        System.out.print("두번째 숫자 : ");
-        b = scan.nextInt();
+        while(true) {
+            System.out.print("첫번째 숫자 : ");
+            a = scan.nextInt();
 
-        System.out.print("+-*/ 중에 고르세요 : ");
-        sign = scan.next();
+            if (1==1) { //scan.hasNextInt()
+                System.out.print("두번째 숫자 : ");
+                b = scan.nextInt();
+                System.out.print("+-*/ 중에 고르세요 : ");
+                sign = scan.next();
+            } else {
+                System.out.println("숫자를 입력하세요");
+            }
 
-        if(sign.equals("+")){
-            System.out.println(a+"+"+b+"="+(a+b));
+            if (sign.equals("+")) {
+                System.out.println(a + "+" + b + "=" + (a + b));
+            } else if (sign.equals("-")) {
+                System.out.println(a + "-" + b + "=" + (a - b));
+            } else if (sign.equals("*")) {
+                System.out.println(a + "*" + b + "=" + (a * b));
+            } else if (sign.equals("/")) {
+                System.out.println(a + "/" + b + "=" + (a / b));
+            } else {
+                System.out.println("부호를 다시 입력하세요!");
+            }
         }
-        else if(sign.equals("-")){
-            System.out.println(a+"-"+b+"="+(a-b));
-        }
-        else if(sign.equals("*")){
-            System.out.println(a+"*"+b+"="+(a*b));
-        }
-        else if(sign.equals("/")){
-            System.out.println(a+"/"+b+"="+(a/b));
-        }
-        else {
-            System.out.println("다시 입력하세요!");
-        }
-
     }
 }
